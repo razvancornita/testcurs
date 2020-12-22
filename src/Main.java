25e45acc127cd60a1343cef5194939e1f783eefb
razvancornita <rrazvan.cornita@gmail.com>
2020-12-22 19:41:47 +0200

    - added new func

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Bye");
+
+        System.out.println("Changing...");
     }
 }
